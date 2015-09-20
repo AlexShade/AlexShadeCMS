c4bdce94ce7357042ae2e165113e22098840a385
AlexShade <RShade@mail.ru>
2015-09-20 23:06:45 +0300

    v 0.0.1

---
+++ ASCMS.Core/Domain/Content/EmptyModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCMS.Domain.Domain.Content
+{
+    public class EmptyModule : ModuleBody
+    {
+        [Key]
+        public int ModuleId { get; set; }
+        public Module ParentModule { get; set; }
+    }
+}
